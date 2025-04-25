700444d5ad65346bdfa76173fb6e8cd0e1f5fbfa
JoGyoungJun <yoop80075@gmail.com>
2025-04-25 10:53:59 +0900

    character crud

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const mongo = new mongodb.MongoClient(process.env.MONGODB,{maxIdleTimeMS: 30*60*1000}).db('lai');
 const rosterClient = mongo.collection('rosters');
+const characterClient = mongo.collection('characters');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -34,5 +35,40 @@ router.delete('/roster', async function(req, res, next) {
   res.send("fail");
 });
 
+router.get('/character', async function(req, res, next) {
+  res.send(await characterClient.find().toArray());
+    }
+)
+
+router.get('/character/search', async function(req, res, next) {
+  res.send(await characterClient.find({name: req.query.characterName}).toArray());
+});
+
+router.post('/character', async function(req, res, next) {
+  const data = await characterClient.insertOne(req.body)
+  res.send("fail");
+});
+
+router.put('/character',async function(req, res, next) {
+  await characterClient.updateOne(
+      { _id: new ObjectId(req.body._id) },
+      {
+        $set:{
+          roster:req.body.roster,
+          name:req.body.name,
+          class:req.body.class,
+          level:req.body.level,
+          lopec:req.body.lopec,
+          zloa:req.body.zloa,
+        }
+      }
+      );
+  res.send("fail");
+})
+
+router.delete('/character', async function(req, res, next) {
+  await characterClient.deleteOne({_id: new ObjectId(req.query._id) })
+  res.send("fail");
+});
 
 module.exports = router;
